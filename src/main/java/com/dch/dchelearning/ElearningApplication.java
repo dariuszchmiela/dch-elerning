e321ed16026b13e189c90cc012a811f5c1528206
DCH <dariuz.chmiela@gmail.com>
2026-06-14 21:22:10 +0200

    Initial commit

---
+++ src/main/java/com/dch/dchelearning/ElearningApplication.java
@@ -0,0 +1,13 @@
+package com.dch.dchelearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElearningApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ElearningApplication.class, args);
+    }
+
+}
