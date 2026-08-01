91d399a333e819cf7d66f508c8b9e5874d05fffe
Dariusz Chmiela <dariusz.chmiela@o2.pl>
2026-08-01 16:41:41 +0200

    Add cors

--- src/main/java/com/dch/dchelearning/config/SecurityFilterChainConfig.java
+++ src/main/java/com/dch/dchelearning/config/SecurityFilterChainConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -16,9 +17,10 @@ public class SecurityFilterChainConfig {
     private static final String LOGIN_PATH = "/api/users/login";
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthFilter) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthFilter, CorsConfigurationSource corsConfigurationSource) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
+            .cors(cors -> cors.configurationSource(corsConfigurationSource))
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(REGISTER_PATH, LOGIN_PATH).permitAll()
