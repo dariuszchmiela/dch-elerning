2d37772080da1843d41e3a5b91c47d1552665ef8
DCH <dariuz.chmiela@gmail.com>
2026-08-14 07:41:36 +0200

    Save progress

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -4,7 +4,14 @@ import { useState } from "react";
 
 const [email, setEmail] = useState();
 const [password, setPassword] = useState();
+const [error, setError] = useState<string | null>(null);
+const [success, setSuccess] = useState(false);
 
 type Role = "STUDENT" | "INSTRUCTOR";
 const [role, setRole] = useState<Role>("STUDENT");
+
+async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    setError(null);
+}
 
