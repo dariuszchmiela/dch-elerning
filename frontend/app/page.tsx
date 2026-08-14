34e6a3f4ec3f3592f546d6050f1f2c5fe9661dcf
DCH <dariuz.chmiela@gmail.com>
2026-08-14 09:25:02 +0200

    Update libs

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
+// TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
+// See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
+export const instant = false;
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
